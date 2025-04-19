baeec302eca60295ecac9b7a35d1b059c83eb08e
“lirizhong97” <lirizhong97@163.com>
2025-04-19 11:12:11 +0800

    Update

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -2223,6 +2223,10 @@ static int ffplay_video_thread(void *arg)
             goto the_end;
         if (!ret)
             continue;
+        if (frame->key_frame) {//Added by lirizhong97
+            av_optimization_decode_err(0);
+            av_optimization_frame_err(0);
+        }
         if(av_optimization_is_decode_err()) {//Added by lirizhong97
             av_log(NULL, AV_LOG_ERROR, "hit decode error.\n");
             continue;
